4504cb5e0f5129ab6a6185e418dcb377cacc2341
Giddy <Idowugidiyanuoluwa@gmail.com>
2022-05-10 14:19:02 +0100

    finished the Jumbotron

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { JumbotronContainer } from "./containers/jumbotron";
+
+const App = () => {
+  return <JumbotronContainer />;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "normalize.css";
 
 import App from "./App";
+import { GlobalStyles } from "./globalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
+    <GlobalStyles />
     <App />
-  </React.StrictMode>
+  </>
+  // </React.StrictMode>
 );
