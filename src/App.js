31ab0a690240ff1b5e7e44471ca76a7ee31c4076
Giddy <Idowugidiyanuoluwa@gmail.com>
2022-06-06 23:48:39 +0100

    edited README file

--- src/App.js
+++ src/App.js
@@ -7,19 +7,41 @@ import {
 } from "react-router-dom";
 import { Home, Browse, Signup, Signin } from "./pages";
 import * as ROUTES from "./constants/routes";
+import { IsUserRedirect, ProtectedRoutes } from "./helpers/routes";
 
 const App = () => {
   const user = {};
   return (
     <Router>
       <Routes>
-        <Route path={ROUTES.BROWSE} element={user ? <Browse /> : <Home />} />
+        {/* <Route path={ROUTES.BROWSE} element={user ? <Browse /> : <Home />} /> */}
+        {/* <Route
+          exact
+          path="/"
+          element={
+            <IsUserRedirect
+              user={user}
+              loggedInPath={ROUTES.BROWSE}
+              path={ROUTES.SIGN_IN}
+            />
+          }
+        /> */}
 
-        <Route path={ROUTES.SIGN_IN} element={<Signin />} />
+        {/* <Route
+          // path={ROUTES.SIGN_IN}
+          element={<IsUserRedirect />}
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+        /> */}
+        <Route path={ROUTES.HOME} element={<ProtectedRoutes />}>
+          <Route path={ROUTES.BROWSE} element={<Browse />} />
+          <Route path={ROUTES.SIGN_IN} element={<Signin />} />
+          <Route path={ROUTES.SIGN_UP} element={<Signup />} />
 
-        <Route path={ROUTES.SIGN_UP} element={<Signup />} />
+          <Route path={ROUTES.HOME} element={<Home />} />
+        </Route>
 
-        <Route path={ROUTES.HOME} element={<Home />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,18 +1,37 @@
-import { Route, Navigate } from "react-router-dom";
+import React from "react";
+import { Routes, Route, useNavigate, Outlet } from "react-router-dom";
 
-export const IsUserRedirect = ({ user, loggedInPath, children, ...rest }) => (
-  <Route
-    {...rest}
-    element={() => {
-      if (!user) {
-        return children;
-      }
+export const IsUserRedirect = ({ user, loggedInPath, children, ...rest }) => {
+  let navigate = useNavigate();
+  return (
+    <Routes>
+      <Route
+        {...rest}
+        element={() => {
+          if (!user) {
+            return children;
+          }
+          if (user) {
+            return navigate(`${loggedInPath}`);
+          }
 
-      if (user) {
-        return <Navigate to={loggedInPath} replace />;
-      }
+          // if (user) {
+          //   return <Navigate to={loggedInPath} replace />;
+          // }
 
-      return null;
-    }}
-  />
-);
+          return null;
+        }}
+      />
+    </Routes>
+  );
+};
+
+const useAuth = () => {
+  return true;
+};
+
+export const ProtectedRoutes = (props) => {
+  const auth = useAuth();
+
+  return auth ? <Outlet /> : <Navigate to="/login" />;
+};
