44154b7e1c633eed2aa66a573051ed94df2641c7
Giddy <Idowugidiyanuoluwa@gmail.com>
2022-06-08 12:08:55 +0100

    query data from firebase

--- src/hooks/use-content.js
+++ src/hooks/use-content.js
@@ -1,14 +1,43 @@
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import { useEffect, useState, useContext } from "react";
 import { FirebaseContext } from "../context/firebase";
+import { db } from "../lib/firebase.prod";
 
 export default function useContent(target) {
-  const [context, setContext] = useState([]);
+  const [content, setContent] = useState([]);
   const { firebase } = useContext(FirebaseContext);
-  useEffect(() => {
-    collection;
 
-    return () => {
-      second;
-    };
+  useEffect(() => {
+    // const docRef = doc(db, "cities", "SF");
+    // const docSnap = await getDoc(docRef);
+
+    // if (docSnap.exists()) {
+    //   console.log("Document data:", docSnap.data());
+    // } else {
+    //   // doc.data() will be undefined in this case
+    //   console.log("No such document!");
+    // }
+
+    const querySnapshot = getDocs(collection(db, target)).then(
+      (querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log(doc.data());
+        });
+        return null;
+      }
+    );
+    //  querySnapshot.forEach((doc) => {
+    //     // doc.data() is never undefined for query doc snapshots
+    //     console.log(doc.id, " => ", doc.data());
+    //   });
+    // setContent(allContent);
+    // .catch((error)=>{
+    //   console.log(error.message);
+    // })
+    // querySnapshot.map((doc) => {
+    //   // doc.data() is never undefined for query doc snapshots
+    //   console.log(doc.id, " => ", doc.data());
+    // });
   }, []);
+  return { [target]: content };
 }
--- src/pages/browse.jsx
+++ src/pages/browse.jsx
@@ -1,5 +1,9 @@
+import { useContent } from "../hooks";
+
 const Browse = () => {
   //TODO: We need the series an films
+  const { series } = useContent("series");
+  console.log(series);
   //TODO: We need slides
   //TODO: Pass it to the browse container
   return <div>Browse</div>;
